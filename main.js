01c9e06006764e0a92346c850628769ef59681be
Greg <projects@gregoire-audoux.fr>
2018-01-21 22:46:45 +0100

    ok ?

--- main.js
+++ main.js
@@ -6,23 +6,45 @@ let zoom = 1;
 let x = 0;
 let y = 0;
 
+
+let ytId = location.hash.slice(1);
+if (!ytId) {
+    console.error('No yt id');
+}
+
+
+console.log(ytId)
+let player = videojs('video', {
+        controls: true,
+        autoplay: true,
+        preload: 'auto',
+        techOrder: ["youtube"],
+        sources: [{"type": "video/youtube", "src": 'https://www.youtube.com/watch?v=' + ytId}]
+
+    })
+;
+
+
+player.play();
+console.log
+
 info.innerText = `zoom ${zoom}`;
 
 body.addEventListener('wheel', (event) => {
-  let _zoom = zoom - event.deltaY / 50;
-  zoom = _zoom < .1 ? .1 : ((_zoom > 2) ? 2 : _zoom)
-  info.innerText = `zoom ${zoom}`;
+    let _zoom = zoom - event.deltaY / 50;
+    zoom = _zoom < .1 ? .1 : ((_zoom > 3) ? 3 : _zoom)
+    info.innerText = `zoom ${zoom}`;
 })
 
 body.addEventListener('mousemove', (event) => {
-  x = event.x - (event.target.clientWidth / 2);
-  y = event.y - (event.target.clientHeight / 2);
+    x = event.x - (event.target.clientWidth / 2);
+    y = event.y - (event.target.clientHeight / 2);
 })
 
 
 function animate() {
-  main.style.transform = `scale(${zoom}) translate(${x}px, ${y}px)`;
-  requestAnimationFrame(animate)
+    main.style.transform = `scale(${zoom}) translate(${x}px, ${y}px)`;
+    requestAnimationFrame(animate)
 }
 
 animate();
